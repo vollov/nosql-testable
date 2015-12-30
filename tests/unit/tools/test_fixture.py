f338cf4dbd7f449ee8b1ea17c739e6fc0a504c5b
Dustin Zhang <dike.zhang@gmail.com>
2015-12-30 11:08:39 -0500

    update sp.py

--- tests/unit/tools/test_fixture.py
+++ tests/unit/tools/test_fixture.py
@@ -17,12 +17,12 @@ class TestFixture(unittest.TestCase):
         ''' method run when every test method terminates'''
         pass
     
-    def test__parse_file_name(self):
-        collection_name = self.fixture._parse_file_name('user.json')
-        expected_collection_name = 'user'
-        self.assertEquals(collection_name,expected_collection_name,'parse_file_name should return ' + expected_collection_name)
+#     def test__parse_file_name(self):
+#         collection_name = self.fixture._parse_file_name('user.json')
+#         expected_collection_name = 'user'
+#         self.assertEquals(collection_name,expected_collection_name,'parse_file_name should return ' + expected_collection_name)
     
     def test__load_json_file(self):
-        data = self.fixture._load_json_file('user.json')
+        data = self.fixture._load_json_file('user')
         expected_size = 3
         self.assertEquals(len(data),expected_size,'collection size should be {0}'.format(expected_size))
--- tools/fixture.py
+++ tools/fixture.py
@@ -11,29 +11,31 @@ class Fixture():
         self.data_dir = os.path.join(base_dir,'tests','data')
         
         
-    def load(self, json_file_name):
+    def load(self, collection_name):
         '''
         Load collection from a json file
         1) parse file_name without json extension
         2) parse file as json objects 
         3) load json file into db
         '''
-        collection_name = self._parse_file_name(json_file_name)
-        docs = self._load_json_file(json_file_name)
-        db_handler = DBManager.get_connection()
-        db_handler[collection_name].insert(docs, safe=True)
         
-    def _parse_file_name(self, file_base_name):
-        '''
-        get collection name: e.g. parse user.json to user
-        '''
-        return os.path.splitext(file_base_name)[0]
+        docs = self._load_json_file(collection_name)
+        print 'import collection = {0}'.format(collection_name)
+        #db_handler = DBManager.get_connection()
+        #db_handler[collection_name].insert(docs, safe=True)
         
-    def _load_json_file(self, json_file_name):
+#     def _parse_file_name(self, file_base_name):
+#         '''
+#         get collection name: e.g. parse user.json to user
+#         '''
+#         return os.path.splitext(file_base_name)[0]
+        
+    def _load_json_file(self, collection_name):
         '''load a json file into memeory
         
         e.g. return data[0]["id"]
         '''
+        json_file_name = collection_name + '.json'
         json_file_path = os.path.join(self.data_dir, json_file_name)
         
         with open(json_file_path) as file_handler:    
